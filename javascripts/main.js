286a7cb42c6b21643b384edacc02148e1375f7bb
Nathan Faucett <nathanfaucett@gmail.com>
2017-11-02 12:29:00 -0500

    added ace editor

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log('This would be the main JS file.');
+(function() {
+    var FONT_SIZE = 16;
+
+    function lineCount(str) {
+        return (str || "").split(/\r|\n/).length;
+    }
+
+    function createAceEditor(style, fontSize) {
+        var divs = document.querySelectorAll(".ace-editor." + style),
+            i, il, div, editor, lines;
+
+        for (i = 0, il = divs.length; i < il; i++) {
+            div = divs[i];
+            editor = ace.edit(div);
+            editor.setTheme("ace/theme/tomorrow");
+            editor.setFontSize(fontSize);
+            editor.setReadOnly(true);
+            editor.getSession().setMode("ace/mode/" + style);
+            editor.container.style.height = (lineCount(editor.getValue()) * (fontSize * 1.2)) + "px";
+        }
+    }
+
+    window.onload = function() {
+        createAceEditor("bash", FONT_SIZE);
+        createAceEditor("javascript", FONT_SIZE);
+    };
+}());
